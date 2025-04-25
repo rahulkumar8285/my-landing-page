f8533adca0bdae244b7606ed938e91bd4835517d
rahulkumar828515 <rahul.kumar@sparsadigital.in>
2025-04-25 23:14:05 +0530

    testing webhock Change

--- include/header.php
+++ include/header.php
@@ -7,7 +7,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>NoirÉlan – Luxury Handbags, Jewelry & More | Elevate Your Style</title>
+    <title>SkinPop – Best Skin Care Porducts</title>
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"/>
     <link rel="stylesheet" href="assets/css/style.css" />
@@ -49,6 +49,7 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
+
 
 
     <!-- Meta Pixel Code -->
